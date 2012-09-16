428712d3c395df5eaeaa9b2415b8317cd2419994
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-09-16 08:38:00 -0400

    ...

--- app/models/garage.rb
+++ app/models/garage.rb
@@ -5,6 +5,7 @@ class Garage < ActiveRecord::Base
   belongs_to :color
   has_many :garage_tags
   has_many :tags, :through => :garage_tags
+  has_many :photos, class_name: "GaragePhoto"
 
   def self.find_with_graph(id)
     where(id: id).joins(:color, :user).includes(:tags).first
--- app/models/garage_photo.rb
+++ app/models/garage_photo.rb
@@ -1,5 +1,6 @@
 class GaragePhoto < ActiveRecord::Base
   attr_accessible :garage_id
+  belongs_to :garage
   has_attached_file :avatar,
       :storage => :s3,
       :s3_credentials => "#{Rails.root}/config/s3.yml",
