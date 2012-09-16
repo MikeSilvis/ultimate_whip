edb729040d2404353513e6e76ed1358ddc5b1daa
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-09-15 23:24:31 -0400

    initial commit of the ultimate whip

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'sqlite3'
+gem 'devise'
+
+group :production do
+  gem 'pg'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'twitter-bootstrap-rails'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'spine-rails'
+end
+gem 'json' '~> 1.7.3'
+
+gem 'jquery-rails'
+gem 'jbuilder'
+gem 'debugger'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UltimateWhip::Application.config.secret_token = 'd38966b1d711fc24b1d40f64daf74db0ba1e1da7306b3642e28afc7876914eb5af98cd3e1504ab0e88eb98fe69756e0b62a6499295d8d05016a0af43eb9c4dbe'
