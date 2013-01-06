639d4d71ceb32a209eb8e0c40e5d9909691aeff8
Mike Silvis <Mike.Silvis@LivingSocial.com>
2013-01-05 21:12:34 -0500

    updating more styles baby!

--- app/controllers/garages_controller.rb
+++ app/controllers/garages_controller.rb
@@ -8,6 +8,9 @@ class GaragesController < ApplicationController
     garage = current_user.vehicles.create(params[:garage])
     if garage.save
       redirect_to new_photo_path, notice: "Your vehicle has been created."
+    else
+      render "garages/new"
+      flash[:alert] = "There was a problem saving your vehicle. The year is probably incorrect"
     end
   end
 
