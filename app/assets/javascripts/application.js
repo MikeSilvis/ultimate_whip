fd9b51e4d5287b41da759c954e918511dd2d8ecc
Mike Silvis <Mike.Silvis@LivingSocial.com>
2013-01-12 20:13:53 -0500

    adding GA tracking

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,9 +26,21 @@ jQuery(function() {
   }
 });
 
+/// Uservoice
 var uvOptions = {};
 (function() {
   var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
   uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/RAA77sJ4WaNpJ6gVLfKSRA.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+})();
+
+/// GA TRACKING
+var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-37625111-1']);
+  _gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
