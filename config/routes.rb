78d0bfaa54be52e4d2b20f3fe82c8321ddd87c2a
Mike Silvis <Mike.Silvis@LivingSocial.com>
2013-01-13 19:56:01 -0500

    whoops broke signup

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ UltimateWhip::Application.routes.draw do
       end
     end
   end
-  resources :users
   resources :photos, only: [:show,:new]
   resources :garage_photos
   resources :makes, only: [:show]
