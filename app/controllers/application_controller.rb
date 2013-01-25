151eb9fcd786866364dc209fab9ab5179b3821f0
Mike Silvis <Mike.Silvis@LivingSocial.com>
2013-01-24 21:42:25 -0500

    fixing registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(user)
     if user.sign_in_count == 1
-      registration_succesful_path
+      registration_succesful_index_path
     else
        root_path
     end
