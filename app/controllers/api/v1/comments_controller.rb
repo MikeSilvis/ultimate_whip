580aa20121d86137319afa3befe06c01ee2e1e38
Mike Silvis <Mike.Silvis@LivingSocial.com>
2013-02-04 14:48:43 -0800

    direct links to profiles in comments

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -5,7 +5,6 @@ class Api::V1::CommentsController < ApiController
     # For now this assumes that only garage_photos can have comments
     photo = GaragePhoto.find(params[:commentable_id])
     comment = photo.comments.create(user_id: current_user.id, comment: params[:message])
-
     UserMailer.notify_photo_comment(comment).deliver
     render json: true
   end
