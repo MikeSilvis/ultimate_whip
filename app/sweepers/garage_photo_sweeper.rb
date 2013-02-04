41a22e2522b38d73a4ab20a6c693cd4e7cc0c91d
Mike Silvis <Mike.Silvis@LivingSocial.com>
2013-02-03 17:26:54 -0800

    fixing cache error

--- app/sweepers/garage_photo_sweeper.rb
+++ app/sweepers/garage_photo_sweeper.rb
@@ -3,7 +3,7 @@ class GaragePhotoSweeper < ActionController::Caching::Sweeper
 
   def sweep(photo)
     expire_page "/api/v1/photos/#{photo.id}"
-    expire_fragment(%r{views\/localhost:3000\/api\/v1\/photos\?&page=\d})
+    expire_fragment(%r{api\/v1\/photos\?&page=\d})
   end
 
   alias_method :after_create, :sweep
