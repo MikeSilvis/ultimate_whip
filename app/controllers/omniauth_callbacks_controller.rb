bfdb68e868217a5d03d7d1a828c9ff05505fbb9f
Paul Silvis <paulsilvis@gmail.com>
2013-07-18 22:07:39 -0700

    added omniauth callback error handling

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,8 +5,11 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 		auth = request.env['omniauth.auth']
 
 		if current_user.link_with_oauth(auth)
-				flash[:success] = I18n.t "devise.omniauth_callbacks.success", :kind => "Instagram"
+			flash[:success] = I18n.t "devise.omniauth_callbacks.success", :kind => "Instagram"
+		else
+			flash[:error] = I18n.t "devise.omniauth_callbacks.failure", :kind => "Instagram"
 		end
+
 		redirect_to users_account_url
 	end
 end
