fb5be2abd92dc50b479337c292399e5ab8cd017a
Mike Silvis <Mike.Silvis@LivingSocial.com>
2013-07-22 01:22:03 -0700

    adding garage sweeper

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module UltimateWhip
 
     config.assets.precompile += %w( active_admin.js active_admin.css active_admin/print.css)
     # Activate observers that should always be running.
-    config.active_record.observers = :tag_sweeper, :garage_photo_sweeper
+    config.active_record.observers = :tag_sweeper, :garage_photo_sweeper, :garage_sweeper
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
