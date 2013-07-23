65ea43f5b770e566e6d56294cafd51004acc0c9c
Mike Silvis <Mike.Silvis@LivingSocial.com>
2013-07-22 19:19:52 -0700

    fixing the url in emails

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,11 +2,4 @@ require 'spec_helper'
 
 describe UsersController do
 
-  describe "GET 'account'" do
-    it "returns http success" do
-      get 'account'
-      response.should be_success
-    end
-  end
-
 end
--- spec/lib/image_scrapper_spec.rb
+++ spec/lib/image_scrapper_spec.rb
@@ -8,13 +8,13 @@ VCR.configure do |c|
 end
 
 describe ImageScrapper do
-  let(:post_url) { "http://www.m3post.com/forums/showthread.php?t=781467" }
+  let(:post_url) { ["http://www.m3post.com/forums/showthread.php?t=781467"] }
 
   context "#find_images_for_page" do
     it "finds all the images that could be relative" do
       VCR.use_cassette('find_images_for_page') do
-        original_post = ImageScrapper.new(post_url).send(:original_post)
-        ImageScrapper.new(post_url).find_images_for_page(original_post).size.should == 15
+        original_post = ImageScrapper.new(post_url, 'body').send(:original_posts)
+        ImageScrapper.new(post_url, 'body').find_images_for_page(original_post.first).size.should == 15
       end
     end
   end
@@ -22,7 +22,7 @@ describe ImageScrapper do
   context "#find_all_images" do
     it "finds all images on the first page and the rest of the pages" do
       VCR.use_cassette('find_all_images') do
-        ImageScrapper.new(post_url).find_all_images.size.should == 15
+        ImageScrapper.new(post_url, 'body').find_all_images.size.should == 15
       end
     end
   end
@@ -30,21 +30,12 @@ describe ImageScrapper do
   context "#valid_image?" do
     it "returns an image larger than 600x500" do
       VCR.use_cassette('valid_image') do
-        ImageScrapper.new(post_url).valid_image?('http://www.ind-distribution.com/images/AT34.jpg').should be_true
+        ImageScrapper.new(post_url, 'body').valid_image?('http://www.ind-distribution.com/images/AT34.jpg').should be_true
       end
     end
     it "returns false an image smaller than 600x500" do
       VCR.use_cassette('invalid_image') do
-        ImageScrapper.new(post_url).valid_image?('http://www.m3post.com/forums/customavatars/avatar74907_1.gif').should_not be_true
-      end
-    end
-  end
-
-  context "#all_pages" do
-    it "combines all of the pages together" do
-      VCR.use_cassette('all_pages') do
-        # Note this number could easily change with the posts on the page increasing
-        ImageScrapper.new(post_url).send(:all_pages).size.should == 4
+        ImageScrapper.new(post_url, 'body').valid_image?('http://www.m3post.com/forums/customavatars/avatar74907_1.gif').should_not be_true
       end
     end
   end
