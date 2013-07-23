d25374577ef667582fdb322ba98d50338abb51ce
Mike Silvis <Mike.Silvis@LivingSocial.com>
2013-07-22 18:08:53 -0700

    adding some dashboard analytics

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,37 @@ ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
-    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "Recent Users" do
+          table_for User.order("created_at DESC").limit(10) do
+            column("Username")        { |user| link_to(user.username, admin_user_path(user)) }
+            column("Registered on")   { |user| user.created_at                       }
+          end
+        end
+      end
+
+      column do
+        panel "Recent Cars" do
+          table_for Garage.order('updated_at desc').limit(10) do
+            column(:type)    { |vehicle| link_to(vehicle.model.name, admin_garage_path(vehicle)) }
+            column(:user) { |vehicle| link_to(vehicle.user.username, admin_user_path(vehicle.user))} 
+            column(:updated_at) { |vehicle| vehicle.updated_at }
+          end
+        end
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel "Recent Photos" do
+          table_for GaragePhoto.order("created_at DESC").limit(10) do
+            column(:photo) { |photo| image_tag photo.photo(:thumb) }
+            column(:id) { |photo| link_to photo.id, admin_garage_photo_path(photo) }
+          end
+        end
+      end
+    end
+  end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
 end
--- workers/image_worker.rb
+++ workers/image_worker.rb
@@ -11,7 +11,7 @@ puts "id is #{params[:id]}"
 images = ImageScrapper.new(params[:urls], params[:div]).find_all_images
 puts images
 
-images.each_slice(10) do |grouped_images|
+images.each_slice(5) do |grouped_images|
   call = Typhoeus.put("#{URL}/api/v1/garages/#{params[:id]}?api=mikeisawesome", params: { images: grouped_images } )
 end
 
