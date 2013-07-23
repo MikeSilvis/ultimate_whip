5254e7b78202dd3ba1cb5d7110b025b28c473011
Mike Silvis <Mike.Silvis@LivingSocial.com>
2013-07-22 19:19:52 -0700

    fixing the url in emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,11 +6,6 @@ class UserMailer < ActionMailer::Base
     @comment = comment
     @to_user = comment.commentable.user
     @from_user = comment.user
-    if @comment.commentable_type == "GaragePhoto"
-      @url = "photos/#{@comment.commentable_id}"
-    else
-      @url = "tags/#{@comment.user.username}"
-    end
 
     mail :to => @to_user.email, subject: "#{@from_user.username} commented on your photo!", :template_name => "notify_photo_comment"
   end
