8b50199e8b2e70a5db0dd964393b6b42d799855f
Mike Silvis <Mike.Silvis@LivingSocial.com>
2013-09-23 18:50:44 -0700

    updating tags to include what is already on the page

--- app/serializers/photo_full_serializer.rb
+++ app/serializers/photo_full_serializer.rb
@@ -5,7 +5,11 @@ class PhotoFullSerializer < ActiveModel::Serializer
   has_one :garage
 
   def tags
-    garage.tags
+    garage.tags - tags_to_reject
+  end
+
+  def tags_to_reject
+    ActsAsTaggableOn::Tag.where('lower(name) = lower(?) or lower(name) = lower(?) or lower(name) = lower(?) or lower(name) = lower(?)', garage.model.name, garage.year.to_s, garage.make.name, garage.user.username)
   end
 
   def cache_key
