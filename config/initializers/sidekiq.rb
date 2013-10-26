fbe6b3f066744c00b7a1ee18040b46ed0c7d6f84
Mike Silvis <Mike.Silvis@LivingSocial.com>
2013-10-25 19:22:18 -0700

    fixing the workers

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,17 +1,6 @@
-#Sidekiq.configure_server do |config|
-  #config.redis = { :url => 'redis://redis.example.com:7372/12' }
-#end
-
-#Sidekiq.configure_client do |config|
-  #config.redis = { :url => 'redis://redis.example.com:7372/12' }
-#end
-
 require 'sidekiq'
+require 'sidekiq/web'
 
-Sidekiq.configure_client do |config|
-  config.redis = { :size => 1 }
-end
-
-Sidekiq.configure_server do |config|
-  config.redis = { :size => 2 }
+Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+  [user, password] == ["auxotic", "auxotic"]
 end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,7 +9,7 @@ before_fork do |server, worker|
     Process.kill 'QUIT', Process.pid
   end
 
-  @sidekiq_pid ||= spawn("bundle exec sidekiq -q paperclip, 5 default")
+  @sidekiq_pid ||= spawn("bundle exec sidekiq -q paperclip -q default")
   t = Thread.new {
     Process.wait(@sidekiq_pid)
     puts "Worker died. Bouncing unicorn."
